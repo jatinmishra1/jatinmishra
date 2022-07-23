27ada92d9321c376a9d18d3771589b3902e981e6
jatinmishra1 <balajimahraaj123@gmail.com>
2022-07-23 11:16:00 +0530

    first commit

---
+++ src/Sadata.jsx
@@ -0,0 +1,19 @@
+const Sadata=[
+{
+    link:"images/sample-5s.mp4"
+},
+{
+link:"images/sample-mp4-file-small.mp4"
+},
+{
+    link:"video.mp4"
+},
+{
+    link:"video2.mp4"
+}
+
+
+
+]
+
+export default Sadata;
--- src/Video.js
+++ src/Video.js
@@ -1,32 +1,53 @@
 import React,{useEffect, useState} from 'react'
-
+import Sadata from './Sadata';
+var link;
 const Video = () => {
-  const [value,setValue]=useState(true);
-  const stop=()=>{
-const marquee=document.getElementById("marquee");
-if(value===false){
-marquee.start();
-setValue(true)
+  const [curr,setcurr]=useState();
+  const [seti,setii]=useState(0);
+// useEffect(()=>{
+//   console.log(curr);
+// setcurr(link);
+// },[myfun])
+
+
+
+
+
+
+function myfun(){
+setcurr(curr=>Sadata[seti].link)
+console.log(curr);
+// setcurr((curr)=>{
+//   curr=Sadata[i].link;
+//   console.log(curr);
+ 
+// });
+
+// console.log(curr);
+setii(seti=>seti+1);
+// console.log(link);
+
+if(seti==Sadata.length){
+  console.log(seti);
+  clearInterval(setmyInt)
 }
-else if(value===true){
-  marquee.stop();
-  setValue(false)
 }
-  }
-  useEffect(()=>{
-    const marquee=document.getElementById("marquee");
-    // marquee.stop();
-  },[])
-  return (
-    <div className='header'>
-<video   autoPlay  loop  onClick={stop} className="video" muted plays-inline>
-          <source src="images/video.mp4" type="video/mp4"></source>
-        </video>
-        <div className='message'>
-        <marquee id="marquee" className="marquee">Hi this is Jatin mishra welocme you all to my news channel</marquee>
-        </div>
-         </div>
-  )
+
+const setmyInt=setInterval(myfun,4000)
+
+return (   
+     
+  <div className='header'>
+    {link}
+<video   autoPlay  loop  className="video" muted plays-inline>    
+ <source src={curr} type="video/mp4"></source>
+      </video>
+      <div className='message'>
+      <marquee id="marquee" className="marquee">Hi this is Jatin mishra welocme you all to my news channel</marquee>
+      </div>
+       </div>
+)
+
 }
 
 export default Video
